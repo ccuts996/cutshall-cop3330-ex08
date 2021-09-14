6f5245819a731fa35e3afea6f49190090fdb28cf
ccuts996 <90644291+ccuts996@users.noreply.github.com>
2021-09-13 20:25:45 -0400

    Initial commit

---
+++ src/main/java/ex08.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ex08 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String people;
+        System.out.print(" How many people? ");
+
+        people = input.next();
+
+        Integer i = Integer.parseInt(people);
+
+        String pizza;
+        System.out.print(" How many pizzas do you have?");
+
+        pizza = input.next();
+
+        Integer j = Integer.parseInt(pizza);
+
+        String slices;
+        System.out.print(" How many slices per pizza?");
+
+        slices = input.next();
+
+        Integer k = Integer.parseInt(slices);
+
+        int totalslices = j * k;
+        int givenslices = totalslices / i;
+        int leftover = totalslices % i;
+        System.out.print(i + " people " + " with " + j + " pizzas " + "(" + totalslices + " total slices)." +System.lineSeparator() );
+        System.out.print("Each person gets " + givenslices + " slices of pizza." + System.lineSeparator());
+        System.out.print("There are " + leftover +" leftover slices of pizza.");
+
+    }
+}
